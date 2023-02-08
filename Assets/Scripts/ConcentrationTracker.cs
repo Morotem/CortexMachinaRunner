7633dfdbbe95a7e70f22cf91bbebc864646e78a3
Nathan Havaux <nathan.havaux@mic-belgique.be>
2023-02-08 15:17:00 +0100

    change renderer of concentrationTracker

--- Assets/Scripts/ConcentrationTracker.cs
+++ Assets/Scripts/ConcentrationTracker.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class ConcentrationTracker : MonoBehaviour
@@ -8,15 +9,17 @@ public class ConcentrationTracker : MonoBehaviour
     public PlayerMovement player;
     public string prefixe = "Concentration : ";
     public TextMeshProUGUI textComponent;
+    public Slider slider;
     // Start is called before the first frame update
     void Start()
     {
-        
+        slider.maxValue = PlayerMovement.MAX_CONCENTRATION_VALUE;
     }
 
     // Update is called once per frame
     void Update()
     {
         textComponent.text = prefixe + player.concentration.ToString();
+        slider.value = player.concentration;
     }
 }
