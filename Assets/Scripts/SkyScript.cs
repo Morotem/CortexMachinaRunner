bb65030ea8ea34c5bed05c3cf114c4420ce3ba5b
Morotem <nathan.hvx@live.fr>
2023-02-13 10:48:54 +0100

    add atmosphere changing

---
+++ Assets/Scripts/SkyScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyScript : MonoBehaviour
+{
+    public Material sky;
+    public PlayerMovement player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        RenderSettings.skybox = sky;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sky.SetFloat("_AtmosphereThickness", (player.concentration/20f));
+        sky.SetFloat("_Exposure", (player.concentration/20f));
+       DynamicGI.UpdateEnvironment();
+    }
+}
