1d2a38e587d88162e3771223e886ff5b7cea615e
DLNBenjamin <delanois_benjamin@hotmail.com>
2023-02-16 14:46:46 +0100

    Change color fire

--- Assets/Scripts/HiddenFireWall.cs
+++ Assets/Scripts/HiddenFireWall.cs
@@ -7,9 +7,12 @@ public class HiddenFireWall : MonoBehaviour
     public PlayerMovement player;
     private bool isStoppingPlayer = false;
     public const float CONCENTRATION_REQUIRED = 40f;
+    public GameObject[] smokeGrey;
+    public GameObject[] smokePurple;
+
     void Start()
     {
-        
+        DesactiveFire();
     }
 
     // Update is called once per frame
@@ -17,19 +20,39 @@ public class HiddenFireWall : MonoBehaviour
     {
         if(!isStoppingPlayer)return;
         if(player.concentration<=CONCENTRATION_REQUIRED){
-            player.MakePlayerRun();
             player.isInFrontOfFire = false;
             isStoppingPlayer = false;
+            player.MakePlayerRun();
+            ActiveFire();
         }
  
     }
 
     void OnTriggerEnter(Collider other) {
-        if(player.concentration<=CONCENTRATION_REQUIRED)return;
+        if(player.concentration<=CONCENTRATION_REQUIRED){
+            ActiveFire();
+            return;}
         player.isInFrontOfFire = true;
         isStoppingPlayer = true;
         player.MakePlayerStop();
         Debug.Log("stop");
+    }
+
+    void ActiveFire(){
+        foreach(GameObject purple in smokePurple){
+            purple.SetActive(true);
+        }
+        foreach(GameObject grey in smokeGrey){
+            grey.SetActive(false);
+        }
+    }
+    void DesactiveFire(){
+        foreach(GameObject purple in smokePurple){
+            purple.SetActive(false);
+        }
+        foreach(GameObject grey in smokeGrey){
+            grey.SetActive(true);
+        }
     }
 
 
