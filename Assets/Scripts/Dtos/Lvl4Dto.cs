970d7d8525a345e4d789af145d84fbac28484119
Morotem <nathan.hvx@live.fr>
2023-02-16 11:33:49 +0100

    Create Lvl4Dto.cs

---
+++ Assets/Scripts/Dtos/Lvl4Dto.cs
@@ -0,0 +1,7 @@
+public class EmotionWeight
+{
+    public int ConcentrationRatio { get; set; }
+    public int RestfullnessWeight { get; set; }
+    public EmotionWeight emotions {get; set;}
+    public MotionWeight motion{get; set;}
+}
