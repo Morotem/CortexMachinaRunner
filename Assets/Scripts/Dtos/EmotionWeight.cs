d2b3937a797133ef7860f78fd11d020274e725e7
Morotem <nathan.hvx@live.fr>
2023-02-16 11:33:32 +0100

    Create EmotionWeight.cs

---
+++ Assets/Scripts/Dtos/EmotionWeight.cs
@@ -0,0 +1,9 @@
+public class EmotionWeight
+{
+    public int Excited { get; set; }
+    public int Happy { get; set; }
+    public int PositiveMood { get; set; }
+    public int NegativeMood { get; set; }
+    public int Sad { get; set; }
+    public int Hate { get; set; }
+}
