8e2911c9f7f516f3afcf469d4178e33cf9da541a
Morotem <nathan.hvx@live.fr>
2023-02-17 11:27:57 +0100

    change camera in SidestepChallenge

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    private Vector3 offset;
+
+    public float smoothSpeed = 0.5f;
+
+    private void Start()
+    {
+ 
+        offset = transform.position - target.position;
+      
+    }
+
+    private void LateUpdate()
+    {
+        SmoothFollow();   
+    }
+
+    public void SmoothFollow()
+    {
+        Vector3 targetPos = target.position + offset;
+        
+        Vector3 newPos = new Vector3(transform.position.x, transform.position.y, targetPos.z);
+        newPos = Vector3.Lerp(transform.position, newPos, smoothSpeed);
+        transform.position = newPos;
+
+    }
+}
