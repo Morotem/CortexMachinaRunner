bd4fcdfb8c13e437cf9fb5e103eaec3d490289b0
DLNBenjamin <121553661+DLNBenjamin@users.noreply.github.com>
2023-02-20 12:01:45 +0100

    Display description

--- Assets/Scripts/HoverButton.cs
+++ Assets/Scripts/HoverButton.cs
@@ -5,11 +5,22 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using UnityEngine.EventSystems;
-public class HoverButton : MonoBehaviour, IPointerEnterHandler
+using TMPro;
+public class HoverButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+
+    public TextMeshProUGUI text;
     public string description;
+    public string nameOfLevel;
     public void OnPointerEnter(PointerEventData eventData)
     {
-        Debug.Log(description);
+        text.text = description;
+        text.fontSize = 14;
+        text.fontStyle = FontStyles.Normal;
+    }
+    public void OnPointerExit(PointerEventData eventData){
+        text.fontStyle = FontStyles.Bold;
+        text.text = nameOfLevel;
+        text.fontSize = 20;
     }
 }
