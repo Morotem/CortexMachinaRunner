9e5cb701246d0815228792f9dd8885ead19432a3
Morotem <nathan.hvx@live.fr>
2023-02-20 10:27:14 +0100

    add script and image gameObject to handle emoji

--- Assets/Scripts/EmotionHandler.cs
+++ Assets/Scripts/EmotionHandler.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class TrailHandler : MonoBehaviour
+public class EmotionHandler : MonoBehaviour
 {
     public ParticleSystem excitedTrail;
     public ParticleSystem happyTrail;
@@ -10,6 +11,13 @@ public class TrailHandler : MonoBehaviour
     public ParticleSystem negativeMoodTrail;
     public ParticleSystem sadTrail;
     public ParticleSystem hateTrail;
+    public Image emojiImage;
+    public Sprite excitedSprite;
+    public Sprite happySprite;
+    public Sprite positiveSprite;
+    public Sprite negativeSprite;
+    public Sprite sadSprite;
+    public Sprite hateSprite;
 
     public EmotionWeight emotions = new EmotionWeight{
         Excited = 0,
@@ -29,38 +37,46 @@ public class TrailHandler : MonoBehaviour
     void Update()
     {
         manageInput();
+        handleEmotionCHange();
+    }
+    void handleEmotionCHange(){
         if(emotions.Excited == 1){
             if(excitedTrail.isPlaying) return;
             desactiveAllTrails();
             excitedTrail.Play();
+            emojiImage.sprite = excitedSprite;
         }
         if(emotions.Happy == 1){
             if(happyTrail.isPlaying) return;
             desactiveAllTrails();
             happyTrail.Play();
+            emojiImage.sprite = happySprite;
         }
         if(emotions.PositiveMood == 1){
             if(positiveMoodTrail.isPlaying) return;
             desactiveAllTrails();
             positiveMoodTrail.Play();
+            emojiImage.sprite = positiveSprite;
         }
         if(emotions.NegativeMood == 1){
             if(negativeMoodTrail.isPlaying) return;
             desactiveAllTrails();
             negativeMoodTrail.Play();
+            emojiImage.sprite = negativeSprite;
         }
         if(emotions.Sad == 1){
             if(sadTrail.isPlaying) return;
             desactiveAllTrails();
             sadTrail.Play();
+            emojiImage.sprite = sadSprite;
         }
         if(emotions.Hate == 1){
             if(hateTrail.isPlaying) return;
             desactiveAllTrails();
             hateTrail.Play();
+            emojiImage.sprite = hateSprite;
         }
     }
-
 
     void desactiveAllTrails(){
         excitedTrail.Stop();
