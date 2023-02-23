da967f9f7e950cdaaee2c0d6f1d6a63437aee335
Morotem <nathan.hvx@live.fr>
2023-02-23 13:14:48 +0100

    amtopshere non-intense

--- Assets/Scripts/EmotionStatePattern/NonIntenseEmotionState.cs
+++ Assets/Scripts/EmotionStatePattern/NonIntenseEmotionState.cs
@@ -4,11 +4,11 @@ public class NonIntenseEmotionState : EmotionState
 {
     private float atmosphereThickness = 0.7f;
     private float skyExposure = 2.07f;
-    private Color skyColor = new Color(51/255, 158/255, 212/255);
+    private Color skyColor = new Color(51f/255, 158f/255, 212f/255);
     private Color groundColor = new Color(125f/255, 125f/255, 125f/255);
-    private Color fogColor = new Color(170/255, 232/255, 255/255);
+    private Color fogColor = new Color(200f/255, 1, 1);
     private Color lightColor = new Color(1, 219f/255, 136f/255);
-    private float fogDensity = 0.005f;
+    private float fogDensity = 0.015f;
     public override void initEnvironnement()
     {
         rain.Play();
